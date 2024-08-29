df4c67bbf00aac476807e87eea9e8018cf461464
vuonghieu <hieuxuanvuong29102000@gmail.com>
2024-08-29 10:24:51 +0700

    Create api: Units - /Find all

---
+++ src/main/java/com/example/csvccdshustbe/repository/units/UnitsRepositoryCustom.java
@@ -0,0 +1,10 @@
+package com.example.csvccdshustbe.repository.units;
+
+import com.example.csvccdshustbe.entity.Units;
+
+import java.util.List;
+
+public interface UnitsRepositoryCustom {
+
+    List<Units> findAllUnits();
+}
---
+++ src/main/java/com/example/csvccdshustbe/service/projects/impl/ProjectsServiceImpl.java
@@ -0,0 +1,8 @@
+package com.example.csvccdshustbe.service.projects.impl;
+
+import com.example.csvccdshustbe.service.projects.ProjectsService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ProjectsServiceImpl implements ProjectsService {
+}
