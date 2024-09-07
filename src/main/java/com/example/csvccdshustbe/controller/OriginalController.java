4b98c03a85930642ef433ab06842fdc6c7a239d4
vuonghieu <hieuxuanvuong29102000@gmail.com>
2024-09-07 10:13:16 +0700

    Create api: create api original find all

--- src/main/java/com/example/csvccdshustbe/controller/OriginalController.java
+++ src/main/java/com/example/csvccdshustbe/controller/OriginalController.java
@@ -18,7 +18,7 @@ import org.webjars.NotFoundException;
 
 @Tag(name = "Original Controller", description = "The Original APIs. Contains operations like find all, create, edit, delete etc.")
 @RestController
-@RequestMapping("/api/v1/asset")
+@RequestMapping("/api/v1/original")
 @PreAuthorize("hasAnyRole('USER','ADMIN')")
 public class OriginalController {
 
