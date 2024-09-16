892a4085e8e6a47ba5407c29e9ba0ac4edb7477b
hieuvx <hieuxuanvuong29102000@gmail.com>
2024-09-16 11:43:07 +0700

    update api department and location

--- src/main/java/com/example/csvccdshustbe/factory/module/impl/HouseModuleFactory.java
+++ src/main/java/com/example/csvccdshustbe/factory/module/impl/HouseModuleFactory.java
@@ -15,7 +15,7 @@ public class HouseModuleFactory implements ModuleFactory {
         HouseModule houseModule = new HouseModule();
         houseModule.setIdAsset(ValueUtil.getIntegerByObject(ValueUtil.getIntegerByObject(mapModuleCreate.get("idAsset"))));
         houseModule.setIsManageGround(ValueUtil.getIntegerByObject(mapModuleCreate.get("isManageGround")));
-        if (ValueUtil.getIntegerByObject(mapModuleCreate.get("isManageHouse")).equals(Constants.HOUSE_MODULES_IS_MANAGE_HOUSE)) {
+        if (houseModule.getIsManageGround().equals(Constants.HOUSE_MODULES_IS_MANAGE_HOUSE)) {
             houseModule.setIdInstance(ValueUtil.getIntegerByObject(mapModuleCreate.get("idInstance")));
         }
         houseModule.setProvinceCode(ValueUtil.getStringByObject(mapModuleCreate.get("provinceCode")));
