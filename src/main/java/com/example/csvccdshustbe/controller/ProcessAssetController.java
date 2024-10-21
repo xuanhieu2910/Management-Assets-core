6291f72b2e54009b231425b15e51619f02cbd5ec
hieuvx <hieuxuanvuong29102000@gmail.com>
2024-10-21 15:44:19 +0700

    fix bug ground module

---
+++ src/main/java/com/example/csvccdshustbe/controller/ProcessAssetController.java
@@ -0,0 +1,14 @@
+package com.example.csvccdshustbe.controller;
+
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Tag(name = "Process Asset Controller", description = "The Process Asset APIs. Contains operations like find all, create, edit, delete etc.")
+@RestController
+@RequestMapping("/api/v1/process-asset")
+public class ProcessAssetController {
+
+
+
+}
