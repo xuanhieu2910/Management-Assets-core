f9ecd7804dacd5844084d2163b81ddde5929e5c6
Trung kiên Phương <kienpt313@gmail.com>
2024-10-22 16:32:53 +0700

    Update API process new

--- src/main/java/com/example/csvccdshustbe/service/dataProcessAsset/impl/DataProcessAssetServiceImpl.java
+++ src/main/java/com/example/csvccdshustbe/service/dataProcessAsset/impl/DataProcessAssetServiceImpl.java
@@ -4,8 +4,13 @@ import com.example.csvccdshustbe.entity.*;
 import com.example.csvccdshustbe.entity.Process;
 import com.example.csvccdshustbe.exception.ValidateFiledException;
 import com.example.csvccdshustbe.repository.dataProcessAsset.DataProcessAssetRepository;
+import com.example.csvccdshustbe.repository.process.ProcessRepository;
 import com.example.csvccdshustbe.request.process.CreateIncreaseAssetRequest;
+import com.example.csvccdshustbe.request.process.FindAllProcessAssetRequest;
+import com.example.csvccdshustbe.response.process.FindAllProcessAssetResponse;
 import com.example.csvccdshustbe.service.dataProcessAsset.DataProcessAssetService;
+import com.example.csvccdshustbe.utility.DateUtil;
+import com.example.csvccdshustbe.utility.PageUtils;
 import com.example.csvccdshustbe.service.document.DocumentService;
 import com.example.csvccdshustbe.service.process.ProcessService;
 import com.example.csvccdshustbe.service.request.RequestService;
@@ -15,14 +20,20 @@ import com.example.csvccdshustbe.service.typeState.TypeStateService;
 import com.example.csvccdshustbe.service.user.CsvcUserService;
 import com.example.csvccdshustbe.utility.Constants;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 import org.springframework.util.CollectionUtils;
 
 import java.util.ArrayList;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Date;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class DataProcessAssetServiceImpl implements DataProcessAssetService {
@@ -70,5 +81,50 @@ public class DataProcessAssetServiceImpl implements DataProcessAssetService {
         if (!CollectionUtils.isEmpty(dataProcessAssets)){
             throw new ValidateFiledException("Exist data process asset!");
         }
+    }
+
+    @Override
+    public Page<FindAllProcessAssetResponse> findAllProcessAsset(FindAllProcessAssetRequest request){
+        Pageable pageable = PageUtils.buildPage(request.getPage(), request.getSize());
+        setIdsDepartmentOriginal(request);
+        setStatusTypeProcess(request);
+        Page<FindAllProcessAssetDto> findAllProcessAssetDtos = dataProcessAssetRepository.findAllProcessAssetDtoByIdsDepartment(request, pageable);
+        return new PageImpl<>(convertToFindAllProcessAssetResponse(findAllProcessAssetDtos.get().collect(Collectors.toList())),
+                pageable, findAllProcessAssetDtos.getTotalElements());
+    }
+
+
+
+    private List<FindAllProcessAssetResponse> convertToFindAllProcessAssetResponse(List<FindAllProcessAssetDto> collect) {
+        List<FindAllProcessAssetResponse> responses = new ArrayList<>();
+        for (FindAllProcessAssetDto dto : collect) {
+            FindAllProcessAssetResponse response = new FindAllProcessAssetResponse();
+            response.setCodeDocument(dto.getCodeDocument());
+            response.setIdUserCreate(dto.getIdUserCreate());
+            response.setCodeUserCreate(dto.getCodeUserCreate());
+            response.setNameUserCreate(dto.getNameUserCreate());
+            response.setStatus(dto.getStatus());
+            response.setTimeIncrease(dto.getTimeIncrease());
+
+            response.setCodeDepartment(dto.getCodeDepartment());
+            response.setNameDepartment(dto.getNameDepartment());
+            response.setTimeCreated(DateUtil.formatToPattern(
+                    DateUtil.formatDatePattern(dto.getTimeCreated(),
+                            DateUtil.DATE_FORMAT),DateUtil.DATE_FORMAT_HH_MM));
+            response.setTimeModified(DateUtil.formatToPattern(
+                    DateUtil.formatDatePattern(dto.getTimeModified(),
+                            DateUtil.DATE_FORMAT),DateUtil.DATE_FORMAT_HH_MM));
+            response.setTimeDocument(dto.getTimeDocument());
+            responses.add(response);
+        }
+        return responses;
+    }
+
+    private void setIdsDepartmentOriginal(FindAllProcessAssetRequest request) {
+        CsvcUser csvcUser = (CsvcUser) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        request.setIdsDepartmentOriginal(csvcUser.getIdsDepartmentCurrent());
+    }
+    private void setStatusTypeProcess(FindAllProcessAssetRequest request) {
+        request.setStatusTypeProcess(1);//test thu = 1 truoc xong xet constant sau
     }
 }
