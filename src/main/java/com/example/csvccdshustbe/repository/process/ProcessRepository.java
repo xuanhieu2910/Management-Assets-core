d1b11ba29f9efece4198dc32a073232ffaf990da
Trung kiên Phương <kienpt313@gmail.com>
2024-10-22 16:32:53 +0700

    Update API process new

---
+++ src/main/java/com/example/csvccdshustbe/repository/process/ProcessRepository.java
@@ -0,0 +1,8 @@
+package com.example.csvccdshustbe.repository.process;
+
+import com.example.csvccdshustbe.entity.Process;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProcessRepository extends JpaRepository<Process, Integer>,ProcessRepositoryCustom{
+}
