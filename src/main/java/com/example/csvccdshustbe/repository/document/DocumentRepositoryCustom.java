57c9cc1075f2d1b7425618de58da21cbe45202eb
Trung kiên Phương <kienpt313@gmail.com>
2024-10-29 15:20:11 +0700

    Update API process new

--- src/main/java/com/example/csvccdshustbe/repository/document/DocumentRepositoryCustom.java
+++ src/main/java/com/example/csvccdshustbe/repository/document/DocumentRepositoryCustom.java
@@ -11,6 +11,7 @@ import java.util.List;
 import java.util.Optional;
 
 public interface DocumentRepositoryCustom {
+
     Optional<Document> findDocumentByCodeAndIdDepartment(String code, Integer idDepartment);
     Optional<Document> findDocumentByIdDepartment(Integer idDepartment);
 
