451486f04e21e97bba76ace3569201b1d9f71b87
hieuvx <hieuxuanvuong29102000@gmail.com>
2024-10-31 11:42:43 +0700

    Update approved

--- src/main/java/com/example/csvccdshustbe/controller/AssetController.java
+++ src/main/java/com/example/csvccdshustbe/controller/AssetController.java
@@ -2,7 +2,10 @@ package com.example.csvccdshustbe.controller;
 
 import com.example.csvccdshustbe.dto.ApiResponseDto;
 import com.example.csvccdshustbe.exception.ValidateFiledException;
-import com.example.csvccdshustbe.request.asset.*;
+import com.example.csvccdshustbe.request.asset.FinaAllAssetToIncreaseRequest;
+import com.example.csvccdshustbe.request.asset.FindAllAssetDocumentRequest;
+import com.example.csvccdshustbe.request.asset.FindAllAssetRequest;
+import com.example.csvccdshustbe.request.asset.FindAllGroundAssetRequest;
 import com.example.csvccdshustbe.service.asset.AssetService;
 import com.example.csvccdshustbe.utility.Constants;
 import com.fasterxml.jackson.core.JsonProcessingException;
