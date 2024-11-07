7afd5f0f67ac15aad436976ebba08937ce4f7380
hieuvx <hieuxuanvuong29102000@gmail.com>
2024-11-07 18:11:40 +0700

    Create api statics inventory

--- src/main/java/com/example/csvccdshustbe/repository/user/impl/CsvcUserRepositoryImpl.java
+++ src/main/java/com/example/csvccdshustbe/repository/user/impl/CsvcUserRepositoryImpl.java
@@ -308,14 +308,14 @@ public class CsvcUserRepositoryImpl implements CsvcUserRepositoryCustom {
     @Override
     public Page<FindAllUserResponse> findAllUser(FindAllUserRequest request, Pageable pageable) {
         StringBuilder sb = new StringBuilder();
-        sb.append(" select csvcUser.code_user, csvcUser.user_name, csvcUser.full_name, " +
-                "       de.id_department, de.name, group_concat(role.short_name separator ';' ) roles,userRole.id_user_role  " +
-                "from csvc_user csvcUser " +
-                "    inner join user_role userRole on csvcUser.id_user = userRole.id_user " +
-                "    inner join role role on userRole.id_role = role.id_role " +
-                "    inner join department de on userRole.id_department = de.id_department " +
-                "where de.id_department in (:idsDepartment) " +
-                "and csvcUser.id_user != :idUserCurrent and  role.title != :titleRole ");
+        sb.append("select * from (select csvcUser.code_user, csvcUser.user_name, csvcUser.full_name,   " +
+                "                        de.id_department, de.name, group_concat(role.short_name separator ';' ) roles,userRole.id_user_role    " +
+                "                 from csvc_user csvcUser   " +
+                "                     inner join user_role userRole on csvcUser.id_user = userRole.id_user   " +
+                "                     inner join role role on userRole.id_role = role.id_role   " +
+                "                     inner join department de on userRole.id_department = de.id_department   " +
+                "                 where de.id_department in (:idsDepartment)   " +
+                "                 and csvcUser.id_user != :idUserCurrent and  role.title != :titleRole  ");
         setConditionFindAllUser(request, sb);
         Query query = entityManager.createNativeQuery(sb.toString());
         setParameterFindAllUser(request, query);
@@ -446,10 +446,10 @@ public class CsvcUserRepositoryImpl implements CsvcUserRepositoryCustom {
     }
 
     private void setParameterFindAllUser(FindAllUserRequest request, Query query) {
-        query.setParameter("titleRole", RolePattern.SuperAdmin.name());
         query.setParameter("idsDepartment", request.getIdsDepartment());
         Integer idUserCurrent = ((CsvcUser) SecurityContextHolder.getContext().getAuthentication().getPrincipal()).getIdUser();
         query.setParameter("idUserCurrent", idUserCurrent);
+        query.setParameter("titleRole", RolePattern.SuperAdmin.name());
         if (StringUtils.isNotBlank(request.getFullName())){
             query.setParameter("fullName", request.getFullName());
         }
@@ -478,7 +478,8 @@ public class CsvcUserRepositoryImpl implements CsvcUserRepositoryCustom {
             sb.append(" and (csvcUser.user_name REGEXP  :keyword) ");
         }
         sb.append(" group by csvcUser.code_user, csvcUser.user_name, csvcUser.full_name, " +
-                "         de.id_department, de.name ");
+                "         de.id_department, de.name, " +
+                "         userRole.id_user_role) as result ");
     }
 
 
