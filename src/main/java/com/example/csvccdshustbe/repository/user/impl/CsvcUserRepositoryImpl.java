4216be5b43adc4be25ac5e6b293c4786f1d680c8
hieuvx <hieuxuanvuong29102000@gmail.com>
2024-11-08 10:08:01 +0700

    Create api statics inventory

--- src/main/java/com/example/csvccdshustbe/repository/user/impl/CsvcUserRepositoryImpl.java
+++ src/main/java/com/example/csvccdshustbe/repository/user/impl/CsvcUserRepositoryImpl.java
@@ -308,16 +308,18 @@ public class CsvcUserRepositoryImpl implements CsvcUserRepositoryCustom {
     @Override
     public Page<FindAllUserResponse> findAllUser(FindAllUserRequest request, Pageable pageable) {
         StringBuilder sb = new StringBuilder();
-        sb.append("select * from (select csvcUser.code_user, csvcUser.user_name, csvcUser.full_name,   " +
-                "                        de.id_department, de.name, group_concat(role.short_name separator ';' ) roles,userRole.id_user_role    " +
-                "                 from csvc_user csvcUser   " +
-                "                     inner join user_role userRole on csvcUser.id_user = userRole.id_user   " +
-                "                     inner join role role on userRole.id_role = role.id_role   " +
-                "                     inner join department de on userRole.id_department = de.id_department   " +
-                "                 where de.id_department in (:idsDepartment)   " +
-                "                 and csvcUser.id_user != :idUserCurrent and  role.title != :titleRole  ");
+        sb.append("select csvcUser.code_user, csvcUser.user_name, csvcUser.full_name,  " +
+                "         de.id_department, de.name,  concat_ws('\\',role.short_name)  as roles, " +
+                "         userRole.id_user_role  " +
+                "   from csvc_user csvcUser          " +
+                "       inner join user_role userRole on csvcUser.id_user = userRole.id_user          " +
+                "       inner join role role on userRole.id_role = role.id_role          " +
+                "       inner join department de on userRole.id_department = de.id_department          " +
+                "   where de.id_department in (:idsDepartment)          " +
+                "   and (csvcUser.id_user != :idUserCurrent and  role.title != :titleRole)  ");
         setConditionFindAllUser(request, sb);
         Query query = entityManager.createNativeQuery(sb.toString());
+        PageUtils.buildQuery(pageable, query);
         setParameterFindAllUser(request, query);
         PageUtils.buildQuery(pageable, query);
         List<Object[]> result = query.getResultList();
@@ -432,13 +434,13 @@ public class CsvcUserRepositoryImpl implements CsvcUserRepositoryCustom {
 
     private long countFindAllUser(FindAllUserRequest request){
         StringBuilder sb = new StringBuilder();
-        sb.append("select count(0) count " +
-                " from csvc_user csvcUser   " +
-                "     inner join user_role userRole on csvcUser.id_user = userRole.id_user   " +
-                "     inner join role role on userRole.id_role = role.id_role   " +
-                "     inner join department de on userRole.id_department = de.id_department   " +
-                " where de.id_department in (:idsDepartment)   " +
-                " and csvcUser.id_user != :idUserCurrent and  role.title != :titleRole  ");
+        sb.append("select count(0)      " +
+                "   from csvc_user csvcUser          " +
+                "       inner join user_role userRole on csvcUser.id_user = userRole.id_user          " +
+                "       inner join role role on userRole.id_role = role.id_role          " +
+                "       inner join department de on userRole.id_department = de.id_department          " +
+                "   where de.id_department in (:idsDepartment)          " +
+                "   and (csvcUser.id_user != :idUserCurrent and  role.title != :titleRole)  ");
         setConditionFindAllUser(request, sb);
         Query query = entityManager.createNativeQuery(sb.toString());
         setParameterFindAllUser(request, query);
@@ -479,7 +481,7 @@ public class CsvcUserRepositoryImpl implements CsvcUserRepositoryCustom {
         }
         sb.append(" group by csvcUser.code_user, csvcUser.user_name, csvcUser.full_name, " +
                 "         de.id_department, de.name, " +
-                "         userRole.id_user_role) as result ");
+                "         userRole.id_user_role ");
     }
 
 
