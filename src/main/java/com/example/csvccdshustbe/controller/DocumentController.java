89a7ce290010feabfec428a24fb5e69e59b9ad18
Trung kiên Phương <kienpt313@gmail.com>
2024-11-21 12:42:41 +0700

    update request excel

--- src/main/java/com/example/csvccdshustbe/controller/DocumentController.java
+++ src/main/java/com/example/csvccdshustbe/controller/DocumentController.java
@@ -1,10 +1,11 @@
 package com.example.csvccdshustbe.controller;
 
 import com.example.csvccdshustbe.dto.ApiResponseDto;
+import com.example.csvccdshustbe.exception.ValidateFiledException;
 import com.example.csvccdshustbe.request.document.FindAllDocumentAssetRequest;
-import com.example.csvccdshustbe.request.process.FindAllProcessAssetIncreaseRequest;
-import com.example.csvccdshustbe.request.process.FindAllProcessAssetInventoryRequest;
+import com.example.csvccdshustbe.request.process.*;
 import com.example.csvccdshustbe.service.document.DocumentService;
+import com.example.csvccdshustbe.service.process.ProcessService;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import net.kaczmarzyk.spring.data.jpa.domain.Like;
 import net.kaczmarzyk.spring.data.jpa.web.annotation.And;
@@ -12,10 +13,7 @@ import net.kaczmarzyk.spring.data.jpa.web.annotation.Spec;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.webjars.NotFoundException;
 
 @Tag(name = "Document Controller", description = "The Document APIs. Contains operations like find all, create, edit, delete etc.")
@@ -26,6 +24,8 @@ public class DocumentController {
     @Autowired
     DocumentService documentService;
 
+    @Autowired
+    ProcessService processService;
     @GetMapping("/generate-code-document")
     public ResponseEntity<?> generateAutoCodeDocument(@RequestParam("tg") String typeGenerate){
         try {
@@ -102,4 +102,60 @@ public class DocumentController {
         }
     }
 
+    @PostMapping("/increase")
+    public ResponseEntity<?> increaseAsset(@RequestBody CreateIncreaseAssetRequest request){
+        try {
+            processService.createIncreaseAsset(request);
+            return ApiResponseDto.createdWithMessage("Create increase asset success!", HttpStatus.OK);
+        } catch (ValidateFiledException e){
+            return ApiResponseDto.createdWithMessage(e.getMessage(), HttpStatus.BAD_REQUEST);
+        } catch (Exception e){
+            return ApiResponseDto.createdWithMessage(e.getMessage(), HttpStatus.BAD_GATEWAY);
+        }
+    }
+    @PostMapping("/inventory")
+    public ResponseEntity<?> createInventory(@RequestBody CreateInventoryAssetRequest request){
+        try {
+            processService.createInventoryAsset(request);
+            return ApiResponseDto.createdWithMessage("Create inventory asset success!", HttpStatus.OK);
+        } catch (ValidateFiledException e){
+            return ApiResponseDto.createdWithMessage(e.getMessage(), HttpStatus.BAD_REQUEST);
+        } catch (Exception e){
+            return ApiResponseDto.createdWithMessage(e.getMessage(), HttpStatus.BAD_GATEWAY);
+        }
+    }
+
+    @GetMapping("/be-assigned")
+    public ResponseEntity<?> findAllProcessBeAssigned(@And({
+            @Spec(path = "page", params = "page", spec = Like.class),
+            @Spec(path = "size", params = "size", spec = Like.class),
+            @Spec(path = "keyword", params = "keyword", spec = Like.class)
+    }) FindAllProcessBeAssignedRequest request){
+        try {
+            return ApiResponseDto.createdWithState(processService.findAllProcessBeAssignedResponse(request),
+                    "Find all process be assigned success!", HttpStatus.OK);
+        } catch (Exception e) {
+            return ApiResponseDto.createdWithMessage(e.getMessage(), HttpStatus.BAD_GATEWAY);
+        }
+    }
+
+    @GetMapping("/statistic-increase")
+    public ResponseEntity<?> getStatisticIncrease(){
+        try {
+            return ApiResponseDto.createdWithState(processService.getStatisticIncrease(),
+                    "Get statistic increase success!", HttpStatus.OK);
+        } catch (Exception e){
+            return ApiResponseDto.createdWithMessage(e.getMessage(), HttpStatus.BAD_GATEWAY);
+        }
+    }
+
+    @GetMapping("/statistic-inventory")
+    public ResponseEntity<?> getStatisticInventory(){
+        try {
+            return ApiResponseDto.createdWithState(processService.getStatisticInventory(),
+                    "Get statistic inventory success!", HttpStatus.OK);
+        } catch (Exception e){
+            return ApiResponseDto.createdWithMessage(e.getMessage(), HttpStatus.BAD_GATEWAY);
+        }
+    }
 }
