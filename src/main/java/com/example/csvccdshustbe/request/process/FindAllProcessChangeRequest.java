64e71819b7b84102649912e0f3059c28a4865ee3
Trung kiên Phương <kienpt313@gmail.com>
2024-11-22 15:37:31 +0700

    update API Document

---
+++ src/main/java/com/example/csvccdshustbe/request/process/FindAllProcessChangeRequest.java
@@ -0,0 +1,22 @@
+package com.example.csvccdshustbe.request.process;
+
+import com.example.csvccdshustbe.request.RequestPageBase;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.List;
+
+@Getter
+@Setter
+public class FindAllProcessChangeRequest extends RequestPageBase {
+    private String codeDocument;
+    private Integer status;
+    private String nameUserCreate;
+    private String timeCreated;
+    private String timeDocument;
+    private String timeChange;
+    private Integer idDepartment;
+    private String nameDepartment;
+
+    private List<Integer> idsDepartmentOriginal;
+}
