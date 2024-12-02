eb53d3aff30f4382a05569e1255c645a2e814f3c
Dung <Dung898949@gmail.com>
2024-12-02 17:34:20 +0700

    create API download Inventory, Revaluation report

--- src/main/java/com/example/csvccdshustbe/service/upload/impl/FileUploadService.java
+++ src/main/java/com/example/csvccdshustbe/service/upload/impl/FileUploadService.java
@@ -381,7 +381,6 @@ public class FileUploadService implements FilesStorageService {
         String reportTime = "Thời điểm kiểm kê " + formattedDate;
         updateCell(sheet, 7, 1, reportTime);
 
-
         if (stakeHoder.isPresent()) {
             List<Object[]> resultListStakeHoder = stakeHoder.get();
             int startingRow = 9;
