a3f262bf0f51f5b24a59dba20eb57e1cd86553f0
hieuvx <hieuxuanvuong29102000@gmail.com>
2024-12-05 17:24:20 +0700

    update api sso azure

--- src/main/java/com/example/csvccdshustbe/utility/FileUtil.java
+++ src/main/java/com/example/csvccdshustbe/utility/FileUtil.java
@@ -24,7 +24,7 @@ public class FileUtil {
     public static final String FOLDER_NAME_IMAGE = "upload_image";
     public static final String FOLDER_NAME_FILE = "upload_file";
     public static final String FOLDER_ASSET = "asset";
-    public static final String FOLDER_NAME_REPORT ="report";
+    public static final String FOLDER_NAME_REPORT ="reports";
     public static final String FOLDER_NAME_SAMPLE_ASSET ="sample_asset";
     public static final String SEPARATOR = "/";
     public static String pathReturn = "";
