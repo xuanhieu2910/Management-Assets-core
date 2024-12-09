a3b5c5a5305c87b40082b8e26a540a116f6796c4
kien-dev <kienpt313@gmail.com>
2024-12-09 16:56:27 +0700

    update api FIND ALL CHILDREN set param isSingle

--- src/main/java/com/example/csvccdshustbe/repository/asset/impl/AssetRepositoryImpl.java
+++ src/main/java/com/example/csvccdshustbe/repository/asset/impl/AssetRepositoryImpl.java
@@ -1901,7 +1901,7 @@ public class AssetRepositoryImpl implements AssetRepositoryCustom {
         if (ObjectUtils.isNotEmpty(request.getIsDecrease())){
             query.setParameter("isDecrease", request.getIsDecrease());
         }
-        if (ObjectUtils.isNotEmpty(request.getIsSingle()) && request.getIsSingle().equals(Boolean.FALSE)){
+        if (ObjectUtils.isNotEmpty(request.getIsSingle())){
             query.setParameter("isSingle", Constants.QUANTITY_DEFAULT);
         }
 
