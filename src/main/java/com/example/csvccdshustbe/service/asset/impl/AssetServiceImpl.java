90eefd9ff8ff7193121f527bf421f713bb84a709
hieuvx <hieuxuanvuong29102000@gmail.com>
2024-12-18 17:36:35 +0700

    update name sheet inventory

--- src/main/java/com/example/csvccdshustbe/service/asset/impl/AssetServiceImpl.java
+++ src/main/java/com/example/csvccdshustbe/service/asset/impl/AssetServiceImpl.java
@@ -1634,7 +1634,7 @@ public class AssetServiceImpl implements AssetService {
         assetInstance.setTimeCreated(dateNow);
         assetInstance.setTimeModified(dateNow);
         assetInstance.setValue(assetRequestsJson);
-        assetInstance.setError(assetErrorRequestsJson);
+        assetInstance.setError(ObjectUtils.isEmpty(assetErrorRequestsJson) ? null : assetErrorRequestsJson);
         assetInstanceRepository.save(assetInstance);
     }
 
