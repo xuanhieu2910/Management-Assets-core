2bec5d7ba4f0fd69795862ebd8b39ce771e7a3c2
hieuvx <hieuxuanvuong29102000@gmail.com>
2024-12-19 12:53:07 +0700

    update name sheet inventory

--- src/main/java/com/example/csvccdshustbe/service/upload/impl/FileUploadService.java
+++ src/main/java/com/example/csvccdshustbe/service/upload/impl/FileUploadService.java
@@ -342,7 +342,7 @@ public class FileUploadService implements FilesStorageService {
     @Override
     public String downLoadFileImportAsset() throws IOException {
         String fileExcel = PropertiesUtil.getProperty("hust.csvc.static.location.resources.static") + SEPARATOR
-                + "Sample_Excel_Import_Asset_root.xlsx";
+                + "Sample_Excel_Import_Asset.xlsx";
 //        String fileExcel = "D:\\CompanyBk\\Sample_Excel_Import_Asset_root.xlsx";
         FileInputStream file = new FileInputStream(new File(fileExcel));
 
